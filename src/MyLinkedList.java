0ec3b1d7feef2b15595b69d9e021378706bf8b04
Izbassar <izok2004@gmail.com>
2023-04-14 21:00:20 +0600

    added head, tail, size parameters, and Node class

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,5 +1,9 @@
 public class MyLinkedList<E> implements List<E> {
+    private class Node {}
 
+    private Node head;
+    private Node tail;
+    private int size;
     @Override
     public int size() {
         return 0;
