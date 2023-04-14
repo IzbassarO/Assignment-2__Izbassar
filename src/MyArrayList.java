0d37211af5537f4794035c4059411b037c393a52
Izbassar <izok2004@gmail.com>
2023-04-14 20:49:42 +0600

    Add initial parameters - elements and size. Also initial capacity as object of a class creates.

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,5 +1,7 @@
 public class MyArrayList<E> implements List<E> {
-
+    private static final int initialCapacity = 10;
+    private Object[] elements;
+    private int size;
     @Override
     public int size() {
         return 0;
