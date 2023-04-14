1c4d96fb75d4f7e90bcf8272fa5299c53a86e25c
Izbassar <izok2004@gmail.com>
2023-04-15 00:08:08 +0600

    Testing of MyLinkedList

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,19 @@ public class Main {
         System.out.println("Element at index 1: " + myList.get(1));
 
         myList.remove(1);
-        System.out.println("Size of myList after removing element at index 1: " + myList.size());
+//        System.out.println("Size of myList after removing element at index 1: " + myList.size());
+
+        System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
+
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        System.out.println("Size of list: " + list.size());
+        System.out.println("Element at index 2: " + list.get(2));
+        //Does not work now remove method
+        myList.remove(2);
+        System.out.println("Size of list: " + list.size());
     }
 }
