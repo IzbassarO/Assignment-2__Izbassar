b51b9de0e45905e4d213a23a7100a78dfdc3100f
Izbassar <izok2004@gmail.com>
2023-04-16 19:15:16 +0600

    Added methods into main class to show that all works!

--- src/Main.java
+++ src/Main.java
@@ -9,19 +9,60 @@ public class Main {
         System.out.println("Element at index 1: " + myList.get(1));
 
         myList.remove(1);
-//        System.out.println("Size of myList after removing element at index 1: " + myList.size());
+        //        System.out.println("Size of myList after removing element at index 1: " + myList.size());
+
+        // Checking if 20 exists in myList object
+        System.out.println("Contains 20: " + myList.contains(20)); // Output: Contains 20: false
+
+        // Sorting the list
+        myList.sort();
+
+        // Clearing the list
+        myList.clear();
+
 
         System.out.println("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
 
-        MyLinkedList<Integer> list = new MyLinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        System.out.println("Size of list: " + list.size());
-        System.out.println("Element at index 2: " + list.get(2));
-        //Does not work now remove method
-        myList.remove(2);
-        System.out.println("Size of list: " + list.size());
+        MyLinkedList<String> list = new MyLinkedList<>();
+
+        // Add elements to the list
+        list.add("apple");
+        list.add("banana");
+        list.add("cherry");
+        list.add("date");
+
+        System.out.println("List after adding elements: " + list);
+
+        // Add cherry at a specific index
+        list.add("cherry", 2);
+        System.out.println("List after adding element at index 2: " + list);
+
+        // Get  element at a specific index
+        System.out.println("Element at index 3: " + list.get(3));
+
+        // Remove an element at a specific index
+        list.remove(1);
+        System.out.println("List after removing element at index 1: " + list);
+
+        // Remove by value
+        list.remove("cherry");
+        System.out.println("List after removing element by value: " + list);
+
+        // Check if the list contains value
+        System.out.println("List contains 'banana': " + list.contains("banana"));
+
+        // Get index
+        System.out.println("Index of 'date': " + list.indexOf("date"));
+
+        // Get the last index
+        System.out.println("Last index of 'grape': " + list.lastIndexOf("grape"));
+
+        // Sort the list
+        list.sort();
+        System.out.println("List after sorting: " + list);
+
+        // Clear object
+        list.clear();
+        System.out.println("List after clearing: " + list);
     }
 }
